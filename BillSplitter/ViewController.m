a2d23ea37f783328bf0b8f1afbb6a67909b0b02f
Alex Lee <x.lee@mail.utoronto.ca>
2017-06-11 01:22:26 -0700

    Accounts for tip percentage, and automatically updates the split total as the user alters their input

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -14,7 +14,15 @@
 
 @property (weak, nonatomic) IBOutlet UISlider *peopleNumberSlider;
 
+@property (weak, nonatomic) IBOutlet UISlider *tipSlider;
+
 @property (weak, nonatomic) IBOutlet UILabel *splitTotalLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *tipPercentDisplay;
+
+@property (weak, nonatomic) IBOutlet UILabel *splitNumberDisplay;
+
+@property (strong, nonatomic)NSNumber *tip;
 
 @end
 
@@ -23,6 +31,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.tip = [[NSNumber alloc] initWithFloat:15.0];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFieldDidChange:) name:UITextFieldTextDidChangeNotification object:nil];
+    
 }
 
 
@@ -31,10 +43,47 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)calculateSplitAmount:(UIButton *)sender {
+- (void)calculateSplitAmount;
+{
     float bill = [self.billTextField.text floatValue];
     float numberOfPeople = floor(self.peopleNumberSlider.value);
-    self.splitTotalLabel.text = [NSString stringWithFormat:@"$%.2lf",bill/numberOfPeople];
+    
+    float total = (bill / numberOfPeople);
+    
+    total += [self updateTipPercent:total];
+    
+    self.splitTotalLabel.text = [NSString stringWithFormat:@"$%.2lf",total];
+}
+
+
+- (IBAction)changedNumberOfPeople:(UISlider *)sender {
+    if (sender == self.peopleNumberSlider) {
+        self.splitNumberDisplay.text = [NSString stringWithFormat:@"%.0lf People",floor(self.peopleNumberSlider.value)];
+        
+        [self calculateSplitAmount];
+    }
+}
+
+- (IBAction)changedTipPercent:(UISlider *)sender {
+    if (sender == self.tipSlider) {
+        
+        self.tip = [NSNumber numberWithFloat:floor(self.tipSlider.value)];
+        
+        self.tipPercentDisplay.text = [NSString stringWithFormat:@"%.0lf%% Tip",[self.tip floatValue]];
+        
+        [self calculateSplitAmount];
+    }
+}
+
+- (float)updateTipPercent:(float)total;
+{
+    float tipPercentage = [self.tip floatValue] / 100;
+    return (total * tipPercentage);
+}
+
+- (void)textFieldDidChange:(NSNotification *)sender;
+{
+    [self calculateSplitAmount];
 }
 
 @end
